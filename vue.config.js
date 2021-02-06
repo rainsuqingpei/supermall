87f8d661bdc0c028ef5a9b9b4f003a6e2475e145
rainsuqingpei <454231850@qq.com>
2021-02-06 14:29:28 +0800

    初始化项目目录

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'components': '@/components',
+				'content': 'components/content',
+				'common': 'components/common',
+				'assets': '@/assets',
+				'common': '@/common',
+				'network': '@/network',
+				'views': '@/views',
+				'store': '@/store'
+			}
+		}
+	}
+}
